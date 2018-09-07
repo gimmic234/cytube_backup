0ba21a2561ece2348f40f60dcd9e0edb26475475
gimmic234 <kagurider430@gmail.com>
2018-09-06 21:41:10 -0400

    channelbase-reopt

--- channelbase-mod.js
+++ channelbase-mod.js
@@ -669,10 +669,10 @@ function scrollerInit() {
 	scrollQueue();
   });
   //new module insert here
-$.getScript("//rawgit.com/BillTube/theme/gh-pages/channels.js");
-$.getScript("//rawgit.com/BillTube/theme/gh-pages/overlay.js");
-$.getScript("//dl.dropbox.com/s/posqswg5ib4pvd8/XaekaiModules.js");
-$.getScript("//dl.dropbox.com/s/x54i2a14jyt58uc/settings.js");
+$.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/channels.min.js");
+$.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/overlay.min.js");
+$.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/XaeKaiModules.js");
+$.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/settings.min.js");
 var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
