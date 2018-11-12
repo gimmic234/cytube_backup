02873444d34feff293c3740d0bde7b440d71401f
gimmic234 <kagurider430@gmail.com>
2018-11-11 20:54:00 -0500

    test2

--- module/channelbase-mod.js
+++ module/channelbase-mod.js
@@ -684,7 +684,7 @@ function scrollerInit() {
   //new module insert here
 $.getScript("//rawgit.com/gimmic234/cytube_backup/f135d324a3b1cd92781d5505f18ba0869896ec96/channels.min.js");
 $.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/overlay.min.js");
-$.getScript("https://raw.githack.com/gimmic234/cytube_backup/96c9c9fcd57bd83ebde631b84fd33163460dd670/module/XaeKaiModules.js");
+$.getScript("https://raw.githack.com/gimmic234/cytube_backup/c38e1b571d48231e088ebe944396339f4d1aebcb/module/XaeKaiModules.js");
 $.getScript("//rawgit.com/gimmic234/cytube_backup/3db6c2cd682c2e9a3ca74406bb75e9cd18f70d10/settings.min.js");
 var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
