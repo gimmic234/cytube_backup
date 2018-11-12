72fd77fdbd0e9d21e052c5b208117ae0d5fc29d4
gimmic234 <kagurider430@gmail.com>
2018-11-11 21:55:34 -0500

    test1

--- current-ext.js
+++ current-ext.js
@@ -1,3 +1,341 @@
+var chatCmdLookup = {
+	'/addq': function(chatCmdText) {
+		if (chatCmdText.length > 1 && chatCmdText.length <= 10) {
+			chatCmdText.shift();
+			chatCmdText.forEach(function(value) {
+				$(document.getElementById('mediaurl')).val(value);
+				$(document.getElementById('queue_end')).click();
+			})
+		}
+	},
+	'/editbg': function(chatCmdText) {
+		if (chatCmdText.length > 1 && rankAdmin) {
+			var url = chatCmdText[1].replace('https:', 'http:');
+			chatCmdText[1] = url;
+			editJs(4, chatCmdText);
+			window.socket.emit("chatMsg", {
+				msg: "background updated"
+			});
+		}
+	},
+	'/savebg1': function(chatCmdText) {
+		if (chatCmdText.length > 1 && rankAdmin) {
+			var url = chatCmdText[1].replace('https:', 'http:');
+			chatCmdText[1] = url;
+			editJs(22, chatCmdText);
+			window.socket.emit("chatMsg", {
+				msg: "bg1 saved " + url
+			});
+		}
+	},
+	'/savebg2': function(chatCmdText) {
+		if (chatCmdText.length > 1 && rankAdmin) {
+			var url = chatCmdText[1].replace('https:', 'http:');
+			chatCmdText[1] = url;
+			editJs(23, chatCmdText);
+			window.socket.emit("chatMsg", {
+				msg: "bg2 saved " + url
+			});
+		}
+	},
+
+	'/savebg3': function(chatCmdText) {
+		if (chatCmdText.length > 1 && rankAdmin) {
+			var url = chatCmdText[1].replace('https:', 'http:');
+			chatCmdText[1] = url;
+			editJs(25, chatCmdText);
+			window.socket.emit("chatMsg", {
+				msg: "bg3 saved " + url
+			});
+		}
+	},
+
+	'/editbanner': function(chatCmdText) {
+		if (chatCmdText.length > 1 && rankAdmin) {
+			var textField = jsTextField.val();
+			var textFieldArray = textField.split("\n");
+			var bannerUrl = chatCmdText[1].replace(/['"]+/g, '').trim();
+			bannerUrl = bannerUrl.replace('https:', 'http:');
+			if (bannerUrl.lastIndexOf('?') > -1) {
+				bannerUrl = bannerUrl.substr(0, bannerUrl.lastIndexOf('?'));
+			}
+			bannerUrl += "?width=1300&height=250";
+			var firstBlock = textFieldArray[2].substr(0, textFieldArray[2].lastIndexOf(' = ') + 1);
+			textField = textField.replace(textFieldArray[2], firstBlock + "= '" + bannerUrl + "';");
+			jsTextField.val(textField);
+			$(document.getElementById('cs-jssubmit')).click();
+		}
+		window.socket.emit("chatMsg", {
+			msg: "banner updated"
+		});
+	},
+	'/purge': function() {
+		if (window.CLIENT.rank >= rankMod) {
+			let list = queueList.children(":visible");
+			deleteAllPlaylist(list);
+		}
+	},
+	'/cdlocal1': function(chatCmdText) {
+		if (chatCmdText.length > 5 && rankAdmin) {
+			if (!(!isNaN(chatCmdText[1]) || !isNaN(chatCmdText[2]) || !isNaN(chatCmdText[3]) || !isNaN(chatCmdText[4]) || !isNaN(chatCmdText[5]))) {
+				window.socket.emit("chatMsg", {
+					msg: "error: invalid countdown input"
+				});
+				return false;
+			}
+			var date = new Date(chatCmdText[1], chatCmdText[2]-1, chatCmdText[3], chatCmdText[4], chatCmdText[5]);
+			var textField = jsTextField.val();
+			var textFieldArray = textField.split("\n");
+			var year = textFieldArray[7].substr(0, textFieldArray[7].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[7], year + ": " + date.getUTCFullYear() + ",");
+			var month = textFieldArray[8].substr(0, textFieldArray[8].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[8], month + ": " + (date.getUTCMonth()+1) + ",");
+			var day = textFieldArray[9].substr(0, textFieldArray[9].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[9], day + ": " + date.getUTCDate() + ",");
+			var hour = textFieldArray[10].substr(0, textFieldArray[10].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[10], hour + ": " + date.getUTCHours() + ",");
+			var minute = textFieldArray[11].substr(0, textFieldArray[11].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[11], minute + ": " + date.getUTCMinutes() + ",");
+
+			jsTextField.val(textField);
+			window.socket.emit("chatMsg", {
+				msg: "countdown date updated"
+			});
+			$(document.getElementById('cs-jssubmit')).click();
+		}
+	},
+
+	'/addemote': function(chatCmdText) {
+		if (chatCmdText.length == 3 && rankMod) {
+			var emote = ':' + chatCmdText[1].replace(/[:]+/g, '') + ':';
+			var emoteUrl = chatCmdText[2]; //.replace('http:', 'https:');
+			if (emoteUrl.lastIndexOf('.gif') > -1) {
+				if (emoteUrl.lastIndexOf('?') > -1) {
+					emoteUrl = emoteUrl.substr(0, emoteUrl.lastIndexOf('?'));
+				}
+			}
+			$(document.getElementById('cs-emotes-newname')).val(emote);
+			$(document.getElementById('cs-emotes-newimage')).val(emoteUrl);
+			$(document.getElementById('cs-emotes-newsubmit')).click();
+			window.socket.emit("chatMsg", {
+				msg: "new emote added " + emote
+			});
+		}
+	},
+
+	'/img': function(chatCmdText) {
+		if (chatCmdText.length == 2) {
+			imgEmote(chatCmdText[1]);
+		}
+	},
+
+	'/skip': function(chatCmdText) {
+		if (rankMod) {
+			var target = $(document.getElementsByClassName('queue_active'));
+			if (target.length > 0) {
+				var name = target.find('.qe_title')[0].innerHTML;
+				/*window.socket.emit("chatMsg", {
+					msg: "removed [" + name + "]"
+				});*/
+				target.find('.qbtn-delete').click();
+			}
+		}
+	},
+	'/cdlocal2': function(chatCmdText) {
+		if (chatCmdText.length > 5 && rankAdmin) {
+			if (!(!isNaN(chatCmdText[1]) || !isNaN(chatCmdText[2]) || !isNaN(chatCmdText[3]) || !isNaN(chatCmdText[4]) || !isNaN(chatCmdText[5]))) {
+				window.socket.emit("chatMsg", {
+					msg: "error: invalid countdown2 input"
+				});
+				return false;
+			}
+
+			var date = new Date(chatCmdText[1], chatCmdText[2]-1, chatCmdText[3], chatCmdText[4], chatCmdText[5]);
+			var textField = jsTextField.val();
+			var textFieldArray = textField.split("\n");
+			var year = textFieldArray[15].substr(0, textFieldArray[15].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[15], year + ": " + date.getUTCFullYear() + ",");
+			var month = textFieldArray[16].substr(0, textFieldArray[16].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[16], month + ": " + (date.getUTCMonth()+1) + ",");
+			var day = textFieldArray[17].substr(0, textFieldArray[17].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[17], day + ": " + date.getUTCDate() + ",");
+			var hour = textFieldArray[18].substr(0, textFieldArray[18].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[18], hour + ": " + date.getUTCHours() + ",");
+			var minute = textFieldArray[19].substr(0, textFieldArray[19].lastIndexOf(': '));
+			textField = textField.replace(textFieldArray[19], minute + ": " + date.getUTCMinutes() + ",");
+
+			jsTextField.val(textField);
+			window.socket.emit("chatMsg", {
+				msg: "countdown2 date updated"
+			});
+			$(document.getElementById('cs-jssubmit')).click();
+		}
+	},
+	'/setbg1': function() {
+		if (rankAdmin) {
+			setAutobg();
+			window.socket.emit("chatMsg", {
+				msg: "loading bg1"
+			});
+		}
+	},
+	'/setbg2': function() {
+		if (rankAdmin) {
+			setAutobg2();
+			window.socket.emit("chatMsg", {
+				msg: "loading bg2"
+			});
+		}
+	},
+
+	'/setbg3': function() {
+		if (rankAdmin) {
+			let textArray = [0, background_img_auto3];
+			editJs(4, textArray);
+			window.socket.emit("chatMsg", {
+				msg: "loading bg3"
+			});
+		}
+	},
+
+	'/muteall': function() {
+		if (rankAdmin) {
+			chatMute = "true";
+			editJs(24, [0, chatMute]);
+			window.socket.emit("chatMsg", {
+				msg: "chat is muted." 
+			});
+		}
+	},
+	'/unmuteall': function() {
+		if (rankAdmin) {
+			chatMute = "false";
+			editJs(24, [0, chatMute]);
+			window.socket.emit("chatMsg", {
+				msg: "chat is unmuted."
+			});
+		}
+	},
+	'/voteratio': function(chatCmdText) {
+		if (rankAdmin) {
+			var ratio = chatCmdText[1];
+			var e = {};
+		    if (!isNaN(ratio)) {
+		       e['voteskip_ratio'] = ratio;
+		       socket.emit("setOptions", e)
+				window.socket.emit("chatMsg", {
+					msg: "voteskip ratio set to " + ratio
+				});
+		    }
+		}
+	},
+	'/maxq': function(chatCmdText) {
+		if (rankAdmin) {
+			var e = {};
+		    try {
+		       e['playlist_max_duration_per_user'] = parseTimeout(chatCmdText[1]);
+		    } catch (e) {
+		       var t = "Invalid timespan value '" + i + "'.  Please use the format HH:MM:SS or enter a single number for the number of seconds.";
+		       alert(t);
+		       return;
+		    }
+			socket.emit("setOptions", e)
+			window.socket.emit("chatMsg", {
+				msg: "max queue time set to " + chatCmdText[1]
+			});
+		}
+	},
+	'/vson': function() {
+		if (rankAdmin) {
+	    	o = {};
+	  		o['allow_voteskip'] = true;
+	  		socket.emit("setOptions", o);
+	  		window.socket.emit("chatMsg", {
+				msg: "voteskip on"
+			});
+  		}
+	},
+	'/vsoff': function() {
+		if (rankAdmin) {
+			o = {};
+	  		o['allow_voteskip'] = false;
+	  		socket.emit("setOptions", o);
+	  		window.socket.emit("chatMsg", {
+				msg: "voteskip off"
+			});
+  		}
+	},
+	'/voteskip': function(chatCmdText) {
+		if ($("#voteskip").attr("disabled")) return;
+		if (!window[CHANNEL.name].audioNotice.Skip.toggleState) return;
+		if (window[CHANNEL.name].audioNotice.Skip.previousCount > 0 && (window[CHANNEL.name].audioNotice.Skip.previousCount+1) == window[CHANNEL.name].audioNotice.Skip.previousNeed) {
+			window.socket.emit("chatMsg", {
+				msg: voteskipMsgFinal
+			});
+			setTimeout(function() {
+				socket.emit("voteskip"), $("#voteskip").attr("disabled", !0);
+			}, 4000);
+		} else {
+			window.socket.emit("chatMsg", {
+			msg: voteskipMsg
+			});
+			socket.emit("voteskip"), $("#voteskip").attr("disabled", !0);
+		}
+	},
+	'/skipclear': function() {
+		$('#voteskipwrap').html('');
+	},
+	'!schwing': function() {
+		imgEmote('http://cdn.discordapp.com/attachments/409829343263719427/497929642347331585/main-qimg-bdbe459c69a03bbd0859657a0c96f9e0.png');
+	},
+	'!rigged': function() {
+		imgEmote('https://images-ext-2.discordapp.net/external/A29xgZ_hAwsgSPzak5tlWkMJwUnYH7kFnixuX2zGWZ8/https/bit.ly/2CX6c8G?width=400&height=225');
+	},
+	'!piano': function() {
+		imgEmote('https://cdn.discordapp.com/attachments/420183063562027008/451551850433478656/2bbhq2.png');
+	},
+	'!rule1': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #1 - |You will never, ever be picked.  Just accept it.|"
+		});
+	},
+	'!rule2': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #2 - |Don't make Mareepy angry.|"
+		});
+	},
+	'!rule3': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #3 - |All server rules still apply. In short, don't be a jerk.|"
+		});
+	},
+	'!rule4': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #4 - |It's fine to dislike a show and voice your opinion on it, but provide constructive criticism on why it's bad rather than \"OMG lul, this show bad\"|"
+		});
+	},
+	'!rule5': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #5 - |You must watch/rewatch the whole series AND discuss it in the server chatroom to qualify for a ticket.|"
+		});
+	},
+	'!rule6': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #6 - |Lying about your participation will be severely penalized, possibly with a suspension from the club.|"
+		});
+	},
+	'!rule7': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #7 - |Don't talk trash about a club pick until we've at least started watching it. Try to give everything a fair chance.|"
+		});
+	},
+	'!rule8': function() {
+		window.socket.emit("chatMsg", {
+				msg: "Club rule #8 - |Pat Poes for good luck!|"
+		});
+	}
+};
+
 var emoteKeyLookup = {
 	13: function(e) {
 		emoteSelectSubmit(e);
