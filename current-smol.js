ab65c4e7a36bc581fbe863d5b6c82a8e825a9012
gimmic234 <kagurider430@gmail.com>
2019-03-25 19:31:09 -0400

    tet

--- current-smol.js
+++ current-smol.js
@@ -142,7 +142,7 @@ window[CHANNEL.name].sequenceList = {
 	'event-ext': {
 		active: 1,
 		rank: -1,
-		url: "https://rawcdn.githack.com/gimmic234/cytube_backup/50a3adb7cfd324c095100bf8648b4651a6fbf5a3/current-ext.js",
+		url: "https://rawcdn.githack.com/gimmic234/cytube_backup/43eca448f53de37dcbf167d9ea591a00f73b3b61/current-ext.js",
 		callback: true
 	},
 	'layout': {
