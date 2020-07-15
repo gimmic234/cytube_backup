ff798076f9f425dbe4736107614b4fd45cdf59c9
gimmic234 <kagurider430@gmail.com>
2020-07-15 14:40:13 -0400

    test

--- module/overlay.js
+++ module/overlay.js
@@ -26,6 +26,7 @@ $("#VideoOverlay").append($("#voteskip"));
 $("#VideoOverlay").append($("#mediarefresh"));
 $("#VideoOverlay").append("<button id='hidechat' title='Hide Chat' class='btn btn-sm btn-default OLB'>Theater Mode</button>");
 $("#VideoOverlay").append("<button id='showchat' title='show Chat' class='btn btn-sm btn-default OLB'>Regular Mode</button>");
+$("#VideoOverlay").append("<button id='pipButton' title='Picture In Picture' class='btn btn-sm btn-default OLB'>PIP</button>");
 $(document).ready(function(){
 	$('#hidechat').on('click', function(){nochat();});
 	$('#showchat').on('click', function(){maxchat();});
@@ -97,3 +98,65 @@ fsVidButton.addEventListener('click', function(e) {
 	e.preventDefault();
 	requestFullscreen(videowrap);
 });
+
+  pipButton.hidden = !document.pictureInPictureEnabled || ytapiplayer_html5_api.disablePictureInPicture;
+
+  pipButton.addEventListener('click', function() {
+    // If there is no element in Picture-in-Picture yet, let's request
+    // Picture-in-Picture for the video, otherwise leave it.
+    if (!document.pictureInPictureElement) {
+      ytapiplayer_html5_api.requestPictureInPicture()
+      .then(pipWindow => {
+        updateVideoSize(pipWindow.width, pipWindow.height);
+        pipWindow.addEventListener('resize', function(event) {
+          updateVideoSize(pipWindow.width, pipWindow.height);
+        });
+      })
+      .catch(error => {
+        console.log(error)
+        // Video failed to enter Picture-in-Picture mode.
+      });
+    } else {
+      document.exitPictureInPicture()
+      .catch(error => {
+        console.error(error)
+        // Video failed to leave Picture-in-Picture mode.
+      });
+    }
+  });
+
+    function updateVideoSize(width, height) {
+    // TODO: Update video size based on pip window width and height.
+  }
+
+  ytapiplayer_html5_api.addEventListener('enterpictureinpicture', function() {
+    // Video element entered Picture-In-Picture mode.
+  });
+
+  ytapiplayer_html5_api.addEventListener('leavepictureinpicture', function() {
+    // Video element left Picture-In-Picture mode.
+  });
+//$("#VideoOverlay").append("<button id='flipButton' title='Flip the Video' class='btn btn-sm btn-default OLB hidden'>Mirror</button>");
+//$("flipButton").click(function() {
+//  $(ytapiplayer_html5_api).toggleClass("flip");
+//});
+
+transcontrols = $('<div id="transcontrols" class="btn-group pull-right" />').appendTo("#VideoOverlay");
+
+	mirrorxbtn = $('<button id="mirrorx-btn" class="btn btn-sm btn-default OLB" title="Mirror X player" />')
+ 	  .html('<span class="glyphicon glyphicon-resize-horizontal"></span>')
+ 	  .appendTo(transcontrols)
+ 	  .on("click", function() {
+		if ($("#ytapiplayer_html5_api").hasClass('mX')) {
+			$("#ytapiplayer_html5_api").removeClass('mX');
+		} else {
+			$("#ytapiplayer_html5_api").addClass('mX');
+		}
+ 	  });
+ var style = document.createElement('style');
+  style.innerHTML = `
+  .mX {
+	-webkit-transform:scaleX(-1); -moz-transform:scaleX(-1); transform:scaleX(-1);
+	-ms-transform:scaleX(-1); -o-transform:scaleX(-1);  }
+  `;
+  document.head.appendChild(style);
